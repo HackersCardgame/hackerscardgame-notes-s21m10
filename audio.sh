e496e4a2ba4a46015812b1e4f1edb103e2b9e4c0
Your Name <you@example.com>
2021-10-21 01:58:39 +0200

    relevant

---
+++ audio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+qjackctl -s  &
+
+sleep 3
+
+pactl load-module module-jack-sink channels=4
+pactl load-module module-jack-source channels=4
+
+pactl load-module module-loopback latency_msec=1
+
+
+#jamulus -c 192.168.192.234:6543 &
+Jamulus -c 192.168.192.234 &
+
+#mixxx &
+
+guitarix &
+
+#obs &
+
+#orca &
+
